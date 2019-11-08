31d7966aa2043e1d51fb247a1e124a1ef0bda5bf
Boris Rorsvort <boris@rorsvort.com>
2019-11-08 16:13:30 +0100

    Add dhis2 icon

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import InfoBoxComponent from "./components/InfoBox";
 import ProgressButtonComponent from "./components/ProgressButton";
 import KeyNumberBlockComponent from "./components/KeyNumberBlock";
 import ExpandableBottomSheetComponent from "./components/ExpandableBottomSheet";
+import Dhis2IconComponent from "./components/Dhis2Icon";
 
 export const InfoBox = InfoBoxComponent;
 export const ProgressButton = ProgressButtonComponent;
 export const KeyNumberBlock = KeyNumberBlockComponent;
 export const ExpandableBottomSheet = ExpandableBottomSheetComponent;
+export const Dhis2Icon = Dhis2IconComponent;
