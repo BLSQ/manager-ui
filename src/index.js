9590937d6d3181f8e087b8525f9e2784125baf11
Boris Rorsvort <boris@rorsvort.com>
2019-11-08 15:39:16 +0100

    Add duo tone chip

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import InfoBoxComponent from "./components/InfoBox";
 import ProgressButtonComponent from "./components/ProgressButton";
 import KeyNumberBlockComponent from "./components/KeyNumberBlock";
 import ExpandableBottomSheetComponent from "./components/ExpandableBottomSheet";
+import DuoToneChipComponent from "./components/DuoToneChip";
 
 export const InfoBox = InfoBoxComponent;
 export const ProgressButton = ProgressButtonComponent;
 export const KeyNumberBlock = KeyNumberBlockComponent;
 export const ExpandableBottomSheet = ExpandableBottomSheetComponent;
+export const DuoToneChip = DuoToneChipComponent;
