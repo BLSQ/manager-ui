2a8172ce72c69b6b0597d25a800685e7e0f20ecb
Boris Rorsvort <boris@rorsvort.com>
2019-11-14 12:06:20 +0100

    build: Add docs addon to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,16 @@
-import { configure } from '@storybook/react';
+import { configure } from "@storybook/react";
+import { addDecorator, addParameters } from "@storybook/react";
+import { jsxDecorator } from "storybook-addon-jsx";
+import { muiTheme } from "storybook-addon-material-ui";
+import theme from "../src/theme";
 
-configure(require.context('../src', true, /\.stories\.js$/), module);
+addDecorator(jsxDecorator);
+addDecorator(muiTheme([theme]));
+
+configure(
+  [
+    require.context("../src", true, /\.stories\.mdx$/),
+    require.context("../src", true, /\.stories\.js$/),
+  ],
+  module,
+);
