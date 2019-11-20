78f55b6f5fefa2c519043ed9f304aa27b6c6d436
Boris Rorsvort <boris@rorsvort.com>
2019-11-20 17:31:12 +0100

    fix: Make Info box dismissable

--- src/components/InfoBox/index.js
+++ src/components/InfoBox/index.js
@@ -1,21 +1,32 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
+import { Typography, IconButton } from "@material-ui/core";
 import Icon from "@material-ui/icons/InfoTwoTone";
-import { Typography } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
 import classNames from "classnames";
+import { useCookie } from "@use-hook/use-cookie";
+import PropTypes from "prop-types";
 
 const useStyles = makeStyles(theme => ({
   root: {
     background: "#efefef",
-    padding: theme.spacing(2),
-    paddingLeft: 0,
+    padding: theme.spacing(2, 4, 2, 0),
     borderRadius: theme.spacing(2) / 2,
     marginBottom: theme.spacing(1),
     display: "flex",
+    position: "relative",
   },
   icon: {
     width: 50,
     color: theme.palette.primary.main,
+  },
+  closeIcon: {
+    position: "absolute",
+    top: theme.spacing(1),
+    right: theme.spacing(1),
+  },
+  smallIcon: {
+    fontSize: 14,
   },
   content: {
     flex: 1,
@@ -24,19 +35,41 @@ const useStyles = makeStyles(theme => ({
 
 const InfoBox = props => {
   const classes = useStyles(props);
-  return (
-    <div className={classNames(classes.root, props.className)}>
-      <Icon className={classes.icon} />
-      <Typography
-        variant="body2"
-        component="div"
-        color="primary"
-        className={classes.content}
-      >
-        {props.children || props.text}
-      </Typography>
-    </div>
+  const [display, setDisplay] = useCookie(
+    `blsq-info-box-hesabu-${props.name}`,
+    "true",
   );
+
+  if (display === "true") {
+    return (
+      <div className={classNames(classes.root, props.className)}>
+        <Icon className={classes.icon} />
+        <Typography
+          variant="body2"
+          component="div"
+          color="primary"
+          className={classes.content}
+        >
+          {props.children || props.text}
+        </Typography>
+        <IconButton
+          size="small"
+          className={classes.closeIcon}
+          onClick={() => setDisplay(false)}
+        >
+          <CloseIcon className={classes.smallIcon} />
+        </IconButton>
+      </div>
+    );
+  } else {
+    return null;
+  }
+};
+
+InfoBox.propTypes = {
+  className: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  text: PropTypes.string,
 };
 
 export default InfoBox;
--- src/components/InfoBox/index.stories.js
+++ src/components/InfoBox/index.stories.js
@@ -7,27 +7,7 @@ export default { title: "Info box" };
 
 export const info = () => (
   <ThemeProvider theme={theme}>
-    <InfoBox>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ipsam
-      sequi tenetur! Sit odio error repellat atque voluptas cum quibusdam
-      facilis, inventore quod odit eligendi deleniti modi ducimus assumenda
-      magnam?
-    </InfoBox>
-  </ThemeProvider>
-);
-export const warning = () => (
-  <ThemeProvider theme={theme}>
-    <InfoBox>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ipsam
-      sequi tenetur! Sit odio error repellat atque voluptas cum quibusdam
-      facilis, inventore quod odit eligendi deleniti modi ducimus assumenda
-      magnam?
-    </InfoBox>
-  </ThemeProvider>
-);
-export const error = () => (
-  <ThemeProvider theme={theme}>
-    <InfoBox>
+    <InfoBox name="demo-example">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ipsam
       sequi tenetur! Sit odio error repellat atque voluptas cum quibusdam
       facilis, inventore quod odit eligendi deleniti modi ducimus assumenda
